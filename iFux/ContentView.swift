fe46ce083b95c81236f8a174f96e4939d924ef6d
Simon Morgenstern <inf2012@lehre.dhbw-stuttgart.de>
2022-04-20 10:18:19 +0200

    Initial Commit

---
+++ iFux/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iFux
+//
+//  Created by Simon Morgenstern on 20.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iFux/iFuxApp.swift
@@ -0,0 +1,17 @@
+//
+//  iFuxApp.swift
+//  iFux
+//
+//  Created by Simon Morgenstern on 20.04.22.
+//
+
+import SwiftUI
+
+@main
+struct iFuxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
